54eb725ec4ac6f08f8fcd91045a2586d05b633de
BluABK <bluabk@bluabk.net>
2021-08-09 16:22:03 +0200

    Added CSDoc, made unmodified vars const and made vars typed

--- Intro/What this code do/Program.cs
+++ Intro/What this code do/Program.cs
@@ -2,14 +2,20 @@
 
 namespace What_this_code_do
 {
+    /**
+     * GET Academy C# Intro Puzzle: "What does this code do?".
+     *
+     * A: It's a character counter. It counts the occurence of characters that are
+     *    ASCII 0-249 and prints a list of counts (edit: now percentage, after task 4).
+     */
     class Program
     {
         static void Main()
         {
-            // Define counts range to most of ASCII table, except the final 5 symbols.
-            var range = 250;
-            // Define an int array counts of size range.
-            var counts = new int[range];
+            // Define counts Range to most of ASCII table, except the final 5 symbols.
+            const int Range = 250;
+            // Define an int array counts of size Range.
+            int[] counts = new int[Range];
             // Define text string and assign it a default value.
             string text = "something";
             // Keep track of total amount of characters (used for %).
@@ -33,19 +39,19 @@ namespace What_this_code_do
                     charTotal++;
                 }
 
-                // Loop over range:
-                for (var i = 0; i < range; i++)
+                // Loop over Range:
+                for (int i = 0; i < Range; i++)
                 {
                     // If item in array counts at index i has entry.
                     if (counts[i] > 0)
                     {
                         // Cast the index value as ASCII to char.
-                        var character = (char)i;
+                        char character = (char)i;
 
                         // Calculate percentage.
                         double charCountPercentage = 100 * (double)counts[i] / charTotal;
 
-                        // Print character and amount to screen.
+                        // Print character and amount to screen, on the form "[X|XX|XXX].XX %".
                         Console.WriteLine($"{character} - {charCountPercentage,FieldWidthRightAligned:F2} %");
                     }
                 }
