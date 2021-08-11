f0cc6c4ca45cb1eba39fa890845a3ff94f2ceaeb
BluABK <bluabk@bluabk.net>
2021-08-11 15:16:50 +0200

    Started on tasks and created a working version of Task2-1

---
+++ Think like a programmer/Pure Puzzles/MyTask2_1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Pure_Puzzles
+{
+    /**
+     * <summary>
+     * 2-1 Write a program that produces the following shape:
+     *   ########
+     *    ######
+     *     ####
+     *      ##
+     *
+     * Given the following limitations:
+     * Only cw allowed is: Console.Write("#"), Console.Write(" ") and Console.WriteLine(...)
+     * </summary>
+     */
+    class MyTask2_1
+    {
+        public void Run()
+        {
+            int symbolCount = 8;
+            int rows = symbolCount / 2;
+            int rowSymbolCount = symbolCount;
+            int paddingCount = 0;
+
+            // For each row.
+            for (int i = 0; i < rows; i++)
+            {
+                // Pad lhs.
+                printPadding( (symbolCount - rowSymbolCount) / 2 );
+
+                // Print current line of symbols.
+                for (int j = 0; j < rowSymbolCount; j++)
+                {
+                    Console.Write("#");
+                }
+
+                // Pad rhs.
+                printPadding((symbolCount - rowSymbolCount) / 2);
+
+                Console.WriteLine();
+
+                rowSymbolCount -= 2;
+                paddingCount += 2;
+
+            }
+        }
+
+        public void printPadding(int paddingCount)
+        {
+            for (int i = 0; i < paddingCount; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ Think like a programmer/Pure Puzzles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pure_Puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTask2_1 task2_1 = new MyTask2_1();
+
+            Console.WriteLine("Task 2-1:");
+            task2_1.Run();
+
+            // Add trailing newline to avoid clutter after exiting.
+            Console.WriteLine();
+        }
+    }
+}
